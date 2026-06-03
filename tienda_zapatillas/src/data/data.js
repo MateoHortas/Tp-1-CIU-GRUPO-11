f92c460863a08e95ee52ab7fdeff0f94c08ce7cf
JuanMazza91 <juance_t@hotmail.com>
2026-06-03 03:30:53 -0300

    Agrego carpeta data con los productos

---
+++ tienda_zapatillas/src/data/data.js
@@ -0,0 +1,511 @@
+export const productos = [
+  /***********************Zapatillas Nike ***********************/
+
+  {
+    id: 1,
+    nombre: "Zapatillas Nike Pegasus 41",
+    categoria: "Running",
+    genero: "Unisex",
+    talle: [38, 39, 40, 41, 42, 43, 44],
+    precio: 129.99,
+    variantes: [
+      {
+        colorName: "Negro",
+
+        imagen: [
+          "./assets/images/nike_pegasus/Pegasus-negra-principal.webp",
+          "./assets/images/nike_pegasus/Pegasus-negra-2.webp",
+          "./assets/images/nike_pegasus/Pegasus-negra-3.webp",
+          "./assets/images/nike_pegasus/Pegasus-negra-4.webp",
+          "./assets/images/nike_pegasus/Pegasus-negra-5.webp",
+        ],
+      },
+      {
+        colorName: "Blanco",
+
+        imagen: [
+          "./assets/images/nike_pegasus/Pegasus-blanca-principal.webp",
+          "./assets/images/nike_pegasus/Pegasus-blanca-2.webp",
+          "./assets/images/nike_pegasus/Pegasus-blanca-3.webp",
+          "./assets/images/nike_pegasus/Pegasus-blanca-4.webp",
+          "./assets/images/nike_pegasus/Pegasus-blanca-5.webp",
+        ],
+      },
+
+      {
+        colorName: "Negro-rosa",
+
+        imagen: [
+          "./assets/images/nike_pegasus/Pegasus-negra-rosa-principal.webp",
+          "./assets/images/nike_pegasus/Pegasus-negra-rosa-2.webp",
+          "./assets/images/nike_pegasus/Pegasus-negra-rosa-3.webp",
+          "./assets/images/nike_pegasus/Pegasus-negra-rosa-4.webp",
+          "./assets/images/nike_pegasus/Pegasus-negra-rosa-5.webp",
+        ],
+      },
+    ],
+
+    descripcion_corta:
+      "Las Nike Pegasus 41 son conocidas por su comodidad y estilo. Con una unidad de aire visible en el talón, estas zapatillas ofrecen una amortiguación excepcional para tus actividades diarias o deportivas.",
+
+    descripcion_larga:
+      "Las Nike Pegasus 41 son una opción popular para aquellos que buscan comodidad y estilo en sus zapatillas. Con una unidad de aire visible en el talón, estas zapatillas ofrecen una amortiguación excepcional que absorbe los impactos y proporciona una sensación de ligereza al caminar o correr. El diseño moderno y versátil de las Nike Pegasus 41 las hace adecuadas para diversas ocasiones, desde actividades deportivas hasta uso casual. Además, su construcción duradera garantiza que estas zapatillas te acompañen durante mucho tiempo, brindándote un rendimiento confiable y un estilo atemporal.",
+    stock: 10,
+    comentarios: [
+      {
+        id: 1,
+        usuario: "Juan123",
+        comentario:
+          "¡Me encantan estas zapatillas! Son súper cómodas y se ven geniales.",
+      },
+      {
+        id: 2,
+        usuario: "SofiaG",
+        comentario:
+          "Las compré para correr y son perfectas. La amortiguación es increíble.",
+      },
+      {
+        id: 3,
+        usuario: "CarlosM",
+        comentario:
+          "Las compré para correr y son perfectas. La amortiguación es increíble.",
+      },
+    ],
+  },
+  {
+    id: 2,
+    nombre: "Nike Structure 26",
+    categoria: "Running",
+    genero: "Unisex",
+    talle: [38, 39, 40, 41, 42, 43, 44],
+    precio: 139.99,
+    variantes: [
+      {
+        colorName: "Negro",
+
+        imagen: [
+          "./assets/images/nike_structure/Structure-negra-principal.webp",
+          "./assets/images/nike_structure/Structure-negra-2.webp",
+          "./assets/images/nike_structure/Structure-negra-3.webp",
+          "./assets/images/nike_structure/Structure-negra-4.webp",
+          "./assets/images/nike_structure/Structure-negra-5.webp",
+        ],
+      },
+      {
+        colorName: "Naranja",
+
+        imagen: [
+          "./assets/images/nike_structure/Structure-naranja-principal.webp",
+          "./assets/images/nike_structure/Structure-naranja-2.webp",
+          "./assets/images/nike_structure/Structure-naranja-3.webp",
+          "./assets/images/nike_structure/Structure-naranja-4.webp",
+          "./assets/images/nike_structure/Structure-naranja-5.webp",
+        ],
+      },
+    ],
+    descripcion_corta:
+      "Las Nike Structure 26 son ideales para corredores que buscan comodidad y soporte. Con su diseño innovador y amortiguación avanzada, estas zapatillas ofrecen un rendimiento excepcional en cada paso.",
+    descripcion_larga:
+      "La amortiguación con soporte de los Structure 26 ofrece estabilidad en las carreras diarias. La entresuela de espuma ReactX Foam de largo completo proporciona una transición amortiguada y estable desde el talón hasta la punta. Además, el sistema de soporte probado en el mediopié envuelve el arco y el talón para proporcionar una plataforma firme.El cuello y la lengüeta suaves, además de la parte superior de malla diseñada estratégicamente proporcionan comodidad, soporte y un ajuste mejorado.Nuestro sistema de soporte del mediopié reduce el índice de eversión del tobillo y proporciona una plataforma estable para la transición y el despegue de la punta.La entresuela con espuma ReactX Foam de largo completo proporciona amortiguación, comodidad y un mayor retorno de energía. Es un 13% más reactiva que la tecnología React anterior.",
+
+    stock: 15,
+    comentarios: [
+      {
+        id: 1,
+        usuario: "LuisR",
+        comentario:
+          "¡Me encantan estas zapatillas! Son súper cómodas y se ven geniales.",
+      },
+      {
+        id: 2,
+        usuario: "AnaP",
+        comentario:
+          "Las compré para correr y son perfectas. La amortiguación es increíble.",
+      },
+      {
+        id: 3,
+        usuario: "MiguelS",
+        comentario:
+          "Las compré para correr y son perfectas. La amortiguación es increíble.",
+      },
+    ],
+  },
+  {
+    id: 3,
+    nombre: "Nike Dunk Low Retro",
+    categoria: "Lifestyle",
+    genero: "Unisex",
+    talle: [38, 39, 40, 41, 42, 43, 44],
+    precio: 109.99,
+    variantes: [
+      {
+        colorName: "Negro-blanco",
+        imagen: [
+          "./assets/images/nike_dunk_low/Dunk-NBlanco-principal.webp",
+          "./assets/images/nike_dunk_low/Dunk-NBlanco-2.webp",
+          "./assets/images/nike_dunk_low/Dunk-NBlanco-3.webp",
+          "./assets/images/nike_dunk_low/Dunk-NBlanco-4.webp",
+          "./assets/images/nike_dunk_low/Dunk-NBlanco-5.webp",
+        ],
+      },
+      {
+        colorName: "Blanco-negro",
+        imagen: [
+          "./assets/images/nike_dunk_low/Dunk-VBlanco-principal.webp",
+          "./assets/images/nike_dunk_low/Dunk-VBlanco-2.webp",
+          "./assets/images/nike_dunk_low/Dunk-VBlanco-3.webp",
+          "./assets/images/nike_dunk_low/Dunk-VBlanco-4.webp",
+          "./assets/images/nike_dunk_low/Dunk-VBlanco-5.webp",
+        ],
+      },
+    ],
+    stock: 8,
+    comentarios: [
+      {
+        id: 1,
+        usuario: "CarlosMini",
+        comentario:
+          "¡Estas zapatillas son increíbles! Muy cómodas y con un diseño genial.",
+      },
+      {
+        id: 2,
+        usuario: "Sofia Galarza",
+        comentario:
+          "Las compré para uso diario y son perfectas. La calidad es excelente.",
+      },
+    ],
+  },
+
+  {
+    id: 4,
+    nombre: "Nike Air Force 1 '07",
+    categoria: "Lifestyle",
+    genero: "Unisex",
+    talle: [38, 39, 40, 41, 42, 43, 44],
+    precio: 119.99,
+    variantes: [
+      {
+        colorName: "Blanco",
+        imagen: [
+          "./assets/images/nike_air_force_1/Air-force-principal.webp",
+          "./assets/images/nike_air_force_1/Air-force-2.webp",
+          "./assets/images/nike_air_force_1/Air-force-3.webp",
+          "./assets/images/nike_air_force_1/Air-force-4.webp",
+          "./assets/images/nike_air_force_1/Air-force-5.webp",
+        ],
+      },
+      {
+        colorName: "Negro",
+        imagen: [
+          "./assets/images/nike_air_force_1/Air-force-negra-principal.webp",
+          "./assets/images/nike_air_force_1/Air-force-negra-2.webp",
+          "./assets/images/nike_air_force_1/Air-force-negra-3.webp",
+          "./assets/images/nike_air_force_1/Air-force-negra-4.webp",
+          "./assets/images/nike_air_force_1/Air-force-negra-5.webp",
+        ],
+      },
+    ],
+    stock: 12,
+    comentarios: [
+      {
+        id: 1,
+        usuario: "AnaP",
+        comentario:
+          "¡Me encantan estas zapatillas! Son súper cómodas y se ven geniales.",
+      },
+      {
+        id: 2,
+
+        usuario: "LuisR",
+        comentario:
+          "Las compré para uso diario y son perfectas. La calidad es excelente.",
+      },
+    ],
+  },
+  /******************************Zapatillas Puma **************************************/
+  {
+    id: 5,
+    nombre: "FAST-R NITRO™ Elite",
+    categoria: "Running",
+    genero: "Hombre",
+    talle: [38, 39, 40, 41, 42, 43, 44],
+    precio: 129.99,
+    variantes: [
+      {
+        colorName: "Verde",
+        imagen: [
+          "./assets/images/puma_fast/fast-principal.avif",
+          "./assets/images/puma_fast/fast-2.avif",
+          "./assets/images/puma_fast/fast-3.avif",
+          "./assets/images/puma_fast/fast-4.avif",
+          "./assets/images/puma_fast/fast-5.avif",
+        ],
+      },
+    ],
+    stock: 5,
+    comentarios: [
+      {
+        id: 1,
+        usuario: "CarlosMini",
+        comentario:
+          "¡Estas zapatillas son increíbles! Muy cómodas y con un diseño genial.",
+      },
+      {
+        id: 2,
+        usuario: "Sofia Galarza",
+        comentario:
+          "Las compré para correr y son perfectas. La amortiguación es increíble.",
+      },
+    ],
+  },
+  {
+    id: 6,
+    nombre: "Magnify NITRO 3",
+    categoria: "Running",
+    genero: "Hombre",
+    talle: [38, 39, 40, 41, 42, 43, 44],
+    precio: 119.99,
+    variantes: [
+      {
+        colorName: "Verde",
+        imagen: [
+          "./assets/images/puma_magnify/Magnify-verde-principal.avif",
+          "./assets/images/puma_magnify/Magnify-verde-2.avif",
+          "./assets/images/puma_magnify/Magnify-verde-3.avif",
+          "./assets/images/puma_magnify/Magnify-verde-4.avif",
+          "./assets/images/puma_magnify/Magnify-verde-5.avif",
+        ],
+      },
+      {
+        colorName: "Negro",
+        imagen: [
+          "./assets/images/puma_magnify/Magnify-negro-principal.avif",
+          "./assets/images/puma_magnify/Magnify-negro-2.avif",
+          "./assets/images/puma_magnify/Magnify-negro-3.avif",
+          "./assets/images/puma_magnify/Magnify-negro-4.avif",
+          "./assets/images/puma_magnify/Magnify-negro-5.avif",
+        ],
+      },
+    ],
+    stock: 7,
+    comentarios: [
+      {
+        id: 1,
+        usuario: "CarlosMini",
+        comentario:
+          "¡Estas zapatillas son increíbles! Muy cómodas y con un diseño genial.",
+      },
+      {
+        id: 2,
+        usuario: "Sofia Galarza",
+        comentario:
+          "Las compré para correr y son perfectas. La amortiguación es increíble.",
+      },
+    ],
+  },
+  {
+    id: 7,
+    nombre: "Deviate NITRO™ Elite 43",
+    categoria: "Running",
+    genero: "Mujer",
+    talle: [38, 39, 40, 41, 42],
+    precio: 150.0,
+    variantes: [
+      {
+        colorName: "Rosa",
+        imagen: [
+          "./assets/images/puma_deviate/Deviate-principal.avif",
+          "./assets/images/puma_deviate/Deviate-2.avif",
+          "./assets/images/puma_deviate/Deviate-3.avif",
+          "./assets/images/puma_deviate/Deviate-4.avif",
+          "./assets/images/puma_deviate/Deviate-5.avif",
+        ],
+      },
+    ],
+    stock: 6,
+    comentarios: [
+      {
+        id: 1,
+        usuario: "Sofia Galarza",
+        comentario:
+          "¡Estas zapatillas son increíbles! Muy cómodas y con un diseño genial.",
+      },
+    ],
+  },
+
+  {
+    id: 8,
+    nombre: "Suede XL Wild Instinct",
+    categoria: "Lifestyle",
+    genero: "Mujer",
+    talle: [36, 37, 38, 39, 40, 41, 42],
+    precio: 89.99,
+    variantes: [
+      {
+        colorName: "Animal print",
+        imagen: [
+          " ./assets/images/puma_suede/Suede-principal.avif",
+          " ./assets/images/puma_suede/Suede-2.avif",
+          " ./assets/images/puma_suede/Suede-3.avif",
+          " ./assets/images/puma_suede/Suede-4.avif",
+          " ./assets/images/puma_suede/Suede-5.avif",
+        ],
+      },
+    ],
+    stock: 0,
+    comentarios: [
+      {
+        id: 1,
+        usuario: "Jazmin Luna",
+        comentario:
+          "Son muy cómodas y tienen un buen diseño, las uso para ir a la universidad y me encantan.",
+      },
+    ],
+  },
+
+  /**********************************Zapatillas Asics************************************/
+  {
+    id: 9,
+    nombre: "Asics Gel Kayano 32 Lite Show M",
+    categoria: "Running",
+    genero: "Hombre",
+    talle: [38, 39, 40, 41, 42, 43, 44],
+    precio: 200.0,
+    variantes: [
+      {
+        colorName: "Negro",
+        imagen: [
+          "./assets/images/asics_kayano_32/Kayano-VNegra-principal.webp",
+          "./assets/images/asics_kayano_32/Kayano-VNegra-2.webp",
+          "./assets/images/asics_kayano_32/Kayano-VNegra-3.webp",
+          "./assets/images/asics_kayano_32/Kayano-VNegra-4.webp",
+          "./assets/images/asics_kayano_32/Kayano-VNegra-5.webp",
+        ],
+      },
+    ],
+    stock: 4,
+    comentarios: [
+      {
+        id: 1,
+        usuario: "CarlosMini",
+        comentario:
+          "¡Estas zapatillas son increíbles! Muy cómodas y con un diseño genial.",
+      },
+      {
+        id: 2,
+        usuario: "Pablo Perez",
+        comentario:
+          "Muy buenas zapatillas para correr, la amortiguación es excelente.",
+      },
+      {
+        id: 3,
+        usuario: "Nicolas nicola",
+        comentario:
+          "Las compré para correr y son perfectas. La amortiguación es increíble.El diseño es muy moderno y me encanta el color",
+      },
+    ],
+  },
+  {
+    id: 10,
+    nombre: "Asics Novablast 5 W",
+    categoria: "Running",
+    genero: "Mujer",
+    talle: [36, 37, 38, 39, 40, 41, 42],
+    precio: 250.0,
+    variantes: [
+      {
+        colorName: "Azul",
+        imagen: [
+          "./assets/images/asics_novablast_5/Novablast-Azul-principal.webp",
+          "./assets/images/asics_novablast_5/Novablast-Azul-2.webp",
+          "./assets/images/asics_novablast_5/Novablast-Azul-3.webp",
+          "./assets/images/asics_novablast_5/Novablast-Azul-4.webp",
+          "./assets/images/asics_novablast_5/Novablast-Azul-5.webp",
+        ],
+      },
+      {
+        colorName: "Rosa",
+        imagen: [
+          "./assets/images/asics_novablast_5/Novablast-rosa-principal.webp",
+          "./assets/images/asics_novablast_5/Novablast-rosa-2.webp",
+          "./assets/images/asics_novablast_5/Novablast-rosa-3.webp",
+          "./assets/images/asics_novablast_5/Novablast-rosa-4.webp",
+          "./assets/images/asics_novablast_5/Novablast-rosa-5.webp",
+        ],
+      },
+    ],
+    stock: 5,
+    comentarios: [
+      {
+        id: 1,
+        usuario: "Sofia Galarza",
+        comentario:
+          "¡Estas zapatillas son increíbles! Muy cómodas y con un diseño genial.",
+      },
+    ],
+  },
+  {
+    id: 11,
+    nombre: "Asics Gel-Trabuco 13",
+    categoria: "Trail",
+    genero: "Hombre",
+    talle: [38, 39, 40, 41, 42, 43, 44],
+    precio: 180.0,
+    variantes: [
+      {
+        colorName: "Rojo",
+        imagen: [
+          "./assets/images/asics_trabuco_13/Trabuco-13-rojo-principal.webp",
+          "./assets/images/asics_trabuco_13/Trabuco-13-rojo-2.webp",
+          "./assets/images/asics_trabuco_13/Trabuco-13-rojo-3.webp",
+          "./assets/images/asics_trabuco_13/Trabuco-13-rojo-4.webp",
+          "./assets/images/asics_trabuco_13/Trabuco-13-rojo-5.webp",
+        ],
+      },
+    ],
+    stock: 3,
+    comentarios: [
+      {
+        id: 1,
+        usuario: "CarlosMini",
+        comentario:
+          "¡Estas zapatillas son increíbles! Muy cómodas y con un diseño genial.",
+      },
+      {
+        id: 2,
+        usuario: "Pablo Pizurno",
+        comentario:
+          "Muy buenas zapatillas para hacer trail, su agarre es excelente.",
+      },
+    ],
+  },
+  {
+    id: 12,
+    nombre: "Asics Trabuco Max 4 WW",
+    categoria: "Trail",
+    genero: "Mujer",
+    talle: [36, 37, 38, 39, 40, 41, 42],
+    precio: 190.0,
+    variantes: [
+      {
+        colorName: "Salmon",
+        imagen: [
+          "./assets/images/asics_trabuco_max_4/Trabuco-max-salmon-principal.webp",
+          "./assets/images/asics_trabuco_max_4/Trabuco-max-salmon-2.webp",
+          "./assets/images/asics_trabuco_max_4/Trabuco-max-salmon-3.webp",
+          "./assets/images/asics_trabuco_max_4/Trabuco-max-salmon-4.webp",
+          "./assets/images/asics_trabuco_max_4/Trabuco-max-salmon-5.webp",
+        ],
+      },
+    ],
+    stock: 1,
+    comentarios: [
+      {
+        id: 1,
+        usuario: "Nicol Sanchez",
+        comentario:
+          "Las zapatillas ofrecen un gran confort, son de la mejor calidad, ideales para hacer varios kilometros en la montaña.",
+      },
+    ],
+  },
+];
